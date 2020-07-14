dee709c65996f86134d9eb88618701604828fdf0
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-14 14:58:43 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,8 @@
-## code your solution here. 
+class Cat 
+#Define a class, called Cat.
+attr_accessor:name
+#Use the attr_accessor macro to create a setter and getter method for a cat's name.
+def meow
+  puts "meow!"
+end
+end
